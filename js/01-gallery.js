0dc3bb6d3940fcd7f5ab0e813dcf90ddba6c0ab0
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-05-05 21:16:02 +0100

    01_lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const itemGallery = document.querySelector('.gallery')
 
-console.log(galleryItems);
+const newGallery = galleryItems.map(
+    item => `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+    />
+    </a>
+    </li>`)
+    .join("")
+itemGallery.insertAdjacentHTML('beforeend', newGallery)
+    
+itemGallery.addEventListener('click', onOpenModal)
+
+function onOpenModal (event){
+event.preventDefault();
+if (event.target.nodeName !== "IMG") {
+    return}
+const imgSource = event.target.getAttribute('data-source')
+const instance = basicLightbox.create(`<img src="${imgSource}"/>`)
+        instance.show()
+}
