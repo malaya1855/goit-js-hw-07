4a544fb661f1a1a64669c995a639eba47f74b1c2
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-05-05 23:48:09 +0100

    lightbox02_creatingGallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const itemGallery = document.querySelector('.gallery')
+
+const newGallery = galleryItems.map(
+    item => `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+       <img class="gallery__image" src="${item.preview}" alt="${item.description}n" />
+    </a>
+ </li>`)
+    .join("")
+itemGallery.insertAdjacentHTML('beforeend', newGallery)
+
+
+
+// event.preventDefault();
