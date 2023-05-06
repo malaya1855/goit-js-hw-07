ac33ee635782392da98565712d078475b2949599
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-05-06 14:04:54 +0100

    02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,17 +1,14 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const itemGallery = document.querySelector('.gallery')
 
 const newGallery = galleryItems.map(
     item => `<li class="gallery__item">
     <a class="gallery__link" href="${item.original}">
-       <img class="gallery__image" src="${item.preview}" alt="${item.description}n" />
+       <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
     </a>
  </li>`)
     .join("")
 itemGallery.insertAdjacentHTML('beforeend', newGallery)
 
-
-
-// event.preventDefault();
+new SimpleLightbox('.gallery a');
