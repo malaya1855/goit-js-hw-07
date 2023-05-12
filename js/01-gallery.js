448156534af6523953b88b0833da56bd22226b1e
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-05-12 23:26:43 +0100

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,34 +1,35 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-const itemGalleryEl = document.querySelector('.gallery')
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+// const itemGalleryEl = document.querySelector('.gallery')
 
-const newGallery = galleryItems.map(
-    item => `<li class="gallery__item">
-    <a class="gallery__link" href="${item.original}">
-    <img
-    class="gallery__image"
-    src="${item.preview}"
-    data-source="${item.original}"
-    alt="${item.description}"
-    />
-    </a>
-    </li>`)
-    .join("")
-itemGalleryEl.insertAdjacentHTML('beforeend', newGallery)
+// const newGallery = galleryItems.map(
+//     item => `<li class="gallery__item">
+//     <a class="gallery__link" href="${item.original}">
+//     <img
+//     class="gallery__image"
+//     src="${item.preview}"
+//     data-source="${item.original}"
+//     alt="${item.description}"
+//     />
+//     </a>
+//     </li>`)
+//     .join("")
+// itemGalleryEl.insertAdjacentHTML('beforeend', newGallery)
 
-document.addEventListener('click', onOpenModal) 
+// document.addEventListener('click', onOpenModal) 
 
-function onOpenModal (event){
-        event.preventDefault();
-    if (event.target.nodeName !== "IMG") {return}
-const instance = basicLightbox.create(`<img src="${event.target.dataset.source}"/>`, 
-{onShow: (instance) => {itemGalleryEl.addEventListener('keydown', onCloseModal)},
+// function onOpenModal (event){
+//         event.preventDefault();
+//     if (event.target.nodeName !== "IMG") {return}
+// const instance = basicLightbox.create(`<img src="${event.target.dataset.source}"/>`, 
+// {onShow: (instance) => {itemGalleryEl.addEventListener('keydown', onCloseModal)},
 
-onClose: (instance) => {itemGalleryEl.removeEventListener('keydown', onCloseModal)}}) 
+// onClose: (instance) => {itemGalleryEl.removeEventListener('keydown', onCloseModal)}}) 
 
-instance.show()
+// instance.show()
 
-function onCloseModal (event) {  
-            instance.close();        
-    }
-}
+// function onCloseModal (event) {  
+//             instance.close();        
+//     }
+// }
+ console.log("hello");
